72c637cfb0277c46da99f8adcf91ec873abff50c
pc <pc@pc-PC>
2018-04-17 17:03:39 +0530

    adddgit

---
+++ gitprogram/src/com/prasannagitdemo/Addgit.java
@@ -0,0 +1,10 @@
+package com.prasannagitdemo;
+
+public class Addgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is very easy");
+	}
+
+}
